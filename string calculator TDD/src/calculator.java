a6545b320afd0c7fa06be50ad1f30899b5310311
GuySh <rakzevel1@gmail.com>
2013-12-12 20:31:12 +0200

    green : TwoNumbers()

--- string calculator TDD/src/calculator.java
+++ string calculator TDD/src/calculator.java
@@ -1,13 +1,21 @@
 
 public class calculator {
-	
+
 	public int Add(String str)
 	{
 		if(str.isEmpty())
 		{
 			return 0;
 		}
-		
-		return Integer.parseInt(str);
+		else
+		{
+			if(str.length() == 1)
+			{
+				return Integer.parseInt(str);
+			}
+			String[] numbers=str.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		}
 	}
+
 }
